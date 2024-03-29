ec8ed59d9923f879ed9f0bcd41e264beb088ce8d
StoicGoose <josh@jwolmedia.com>
2024-03-29 10:40:15 -0600

    Project Initialized with TailwindCSS, Initial Header Component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<nav>
+	<ul class="flex justify-between px-4 lg:px-10">
+		{#if $page.route.id === '/'}
+			<a href="/menu" class="text-blue-500">MENU</a>
+		{:else}
+			<a href="/">HOME</a>
+		{/if}
+		<button class={$page.route.id === '/' ? 'text-blue-500' : ''}>CART</button>
+	</ul>
+</nav>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+	import Header from '$lib/components/Header.svelte';
+	import AnimatedRoute from '$lib/components/AnimatedRoute.svelte';
+</script>
+
+<Header />
+<AnimatedRoute>
+	<slot />
+</AnimatedRoute>
